1a99ed40e54a7f3c269c804d186abd0fab8e4134
belyaev-ay <anton.belyaev@sibit.tech>
2024-04-11 14:19:39 +0700

    First emulation

---
+++ src/main/java/pw/avvero/GameOfLife.java
@@ -0,0 +1,86 @@
+package pw.avvero;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GameOfLife {
+
+    private int[][] board;
+
+    public GameOfLife(int x, int y) {
+        this.board = new int[x][y];
+        initializeRandom(board);
+    }
+
+    public void next() {
+        this.board = nextGeneration(board);
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    private int[][] nextGeneration(int[][] board) {
+        int[][] next = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int n = neighbours(board, i, j);
+                if (board[i][j] == 1) {
+                    if (n < 2) {
+                        next[i][j] = 0;
+                    } else if (n < 4) {
+                        next[i][j] = 1;
+                    } else {
+                        next[i][j] = 0;
+                    }
+                } else {
+                    if (n == 3) {
+                        next[i][j] = 1;
+                    }
+                }
+            }
+        }
+        return next;
+    }
+
+    private int neighbours(int[][] board, int i, int j) {
+        int result = 0;
+        for (int x = -1; x < 2; x++) {
+            for (int y = -1; y < 2; y++) {
+                if (x == 0 && y == 0) continue;
+                result += exists(board, i + x, j + y) ? 1 : 0;
+            }
+        }
+        return result;
+    }
+
+    private boolean exists(int[][] board, int i, int j) {
+        if(i < 0 || i == board.length || j < 0 || j == board[i].length) return false;
+        return board[i][j] == 1;
+    }
+
+
+    private void initializeV(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (i == j) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    private int[][] nextGenerationRandom(int[][] board) {
+        int i = ThreadLocalRandom.current().nextInt(0, board.length);
+        int j = ThreadLocalRandom.current().nextInt(0, board[i].length);
+        board[i][j] = board[i][j] ^ 1;
+        return board;
+    }
+
+    private void initializeRandom(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = ThreadLocalRandom.current().nextBoolean() ? 1 : 0;
+            }
+        }
+    }
+}
--- src/main/java/pw/avvero/Main.java
+++ src/main/java/pw/avvero/Main.java
@@ -1,89 +1,21 @@
 package pw.avvero;
 
 import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         int x = 50, y = 100;
-        int[][] board = new int[x][y];
-        // Seed
-//        initializeV(board);
-        initializeRandom(board);
+        GameOfLife life = new GameOfLife(50, 100);
         clear();
-        System.out.println(toString(board));
+        System.out.println(toString(life.getBoard()));
         Thread.sleep(100);
         while (true) {
-            board = nextGeneration(board);
+            life.next();
             //
             clear();
-            System.out.println(toString(board));
+            System.out.println(toString(life.getBoard()));
             //
             Thread.sleep(200);
-        }
-    }
-
-    private static int[][] nextGeneration(int[][] board) {
-        int[][] next = new int[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                int n = neighbours(board, i, j);
-                if (board[i][j] == 1) {
-                    if (n < 2) {
-                        next[i][j] = 0;
-                    } else if (n < 4) {
-                        next[i][j] = 1;
-                    } else {
-                        next[i][j] = 0;
-                    }
-                } else {
-                    if (n == 3) {
-                        next[i][j] = 1;
-                    }
-                }
-            }
-        }
-        return next;
-    }
-
-    private static int neighbours(int[][] board, int i, int j) {
-        int result = 0;
-        for (int x = -1; x < 2; x++) {
-            for (int y = -1; y < 2; y++) {
-                if (x == 0 && y == 0) continue;
-                result += exists(board, i + x, j + y) ? 1 : 0;
-            }
-        }
-        return result;
-    }
-
-    private static boolean exists(int[][] board, int i, int j) {
-        if(i < 0 || i == board.length || j < 0 || j == board[i].length) return false;
-        return board[i][j] == 1;
-    }
-
-    private static int[][] nextGenerationRandom(int[][] board) {
-        int i = ThreadLocalRandom.current().nextInt(0, board.length);
-        int j = ThreadLocalRandom.current().nextInt(0, board[i].length);
-        board[i][j] = board[i][j] ^ 1;
-        return board;
-    }
-
-    private static void initializeRandom(int[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = ThreadLocalRandom.current().nextBoolean() ? 1 : 0;
-            }
-        }
-    }
-
-    private static void initializeV(int[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (i == j) {
-                    board[i][j] = 1;
-                }
-            }
         }
     }
 
@@ -109,6 +41,7 @@ public class Main {
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
             }
-        } catch (IOException | InterruptedException ex) {}
+        } catch (IOException | InterruptedException ex) {
+        }
     }
 }
