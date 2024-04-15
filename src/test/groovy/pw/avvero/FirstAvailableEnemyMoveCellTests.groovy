c3955fdc1dd2f14af0b7f37779cd59695e2e88b2
belyaev-ay <anton.belyaev@sibit.tech>
2024-04-15 12:44:21 +0700

    Design on claims

--- src/test/groovy/pw/avvero/FirstAvailableEnemyMoveCellTests.groovy
+++ src/test/groovy/pw/avvero/FirstAvailableEnemyMoveCellTests.groovy
@@ -3,6 +3,8 @@ package pw.avvero
 import pw.avvero.board.Board
 import pw.avvero.board.BoardBordered
 import pw.avvero.board.Cell
+import pw.avvero.board.MoorNeighborhood
+import pw.avvero.board.VonNeumannNeighborhood
 import pw.avvero.move.FirstAvailableEnemyMoveCell
 import pw.avvero.move.MoveTarget
 import pw.avvero.move.Pawn
@@ -14,60 +16,109 @@ import java.util.concurrent.atomic.AtomicInteger
 class FirstAvailableEnemyMoveCellTests extends Specification {
 
     @Unroll
-    def "Move tests for two enemies"() {
+    def "Move tests for two enemies 4x4"() {
         when:
-        Board<MoveTarget> board = new BoardBordered<>(4, 4, FirstAvailableEnemyMoveCell::new)
-        AtomicInteger ids = new AtomicInteger(1);
-        board.update(0, 0, (current) -> current.value = new Pawn(ids.getAndIncrement()))
-        board.update(3, 3, (current) -> current.value = new Pawn(ids.getAndIncrement()))
+        Board<MoveTarget> boardM = new BoardBordered<>(4, 4, new MoorNeighborhood<>(), FirstAvailableEnemyMoveCell::new)
+        AtomicInteger idsm = new AtomicInteger(1);
+        boardM.update(0, 0, (current) -> current.value = new Pawn(idsm.getAndIncrement()))
+        boardM.update(3, 3, (current) -> current.value = new Pawn(idsm.getAndIncrement()))
+        and:
+        Board<MoveTarget> boardV = new BoardBordered<>(4, 4, new VonNeumannNeighborhood<>(), FirstAvailableEnemyMoveCell::new)
+        AtomicInteger idsv = new AtomicInteger(1);
+        boardV.update(0, 0, (current) -> current.value = new Pawn(idsv.getAndIncrement()))
+        boardV.update(3, 3, (current) -> current.value = new Pawn(idsv.getAndIncrement()))
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""1000
-                                                              0000
-                                                              0000
-                                                              0002""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""1000
+                                                               0000
+                                                               0000
+                                                               0002""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""1000
+                                                               0000
+                                                               0000
+                                                               0002""")
         when:
-        board.nextCycle()
+        boardM.nextCycle()
+        boardV.nextCycle()
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""0000
-                                                              0100
-                                                              0020
-                                                              0000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0000
+                                                               0100
+                                                               0020
+                                                               0000""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""0000
+                                                               1000
+                                                               0002
+                                                               0000""")
         when:
-        board.nextCycle()
+        boardM.nextCycle()
+        boardV.nextCycle()
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""0000
-                                                              0100
-                                                              0000
-                                                              0000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0000
+                                                               0100
+                                                               0000
+                                                               0000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0000
+                                                               0100
+                                                               0000
+                                                               0000""")
     }
 
-
     @Unroll
-    def "Move tests for three enemies"() {
+    def "Move tests for two enemies 4x7"() {
         when:
-        Board<MoveTarget> board = new BoardBordered<>(4, 7, FirstAvailableEnemyMoveCell::new)
-        AtomicInteger ids = new AtomicInteger(1);
-        board.update(1, 0, (current) -> current.value = new Pawn(ids.getAndIncrement()))
-        board.update(2, 6, (current) -> current.value = new Pawn(ids.getAndIncrement()))
+        Board<MoveTarget> boardM = new BoardBordered<>(4, 7, new MoorNeighborhood<>(), FirstAvailableEnemyMoveCell::new)
+        AtomicInteger idsm = new AtomicInteger(1);
+        boardM.update(1, 0, (current) -> current.value = new Pawn(idsm.getAndIncrement()))
+        boardM.update(2, 6, (current) -> current.value = new Pawn(idsm.getAndIncrement()))
+        and:
+        Board<MoveTarget> boardV = new BoardBordered<>(4, 7, new VonNeumannNeighborhood<>(), FirstAvailableEnemyMoveCell::new)
+        AtomicInteger idsv = new AtomicInteger(1);
+        boardV.update(1, 0, (current) -> current.value = new Pawn(idsv.getAndIncrement()))
+        boardV.update(2, 6, (current) -> current.value = new Pawn(idsv.getAndIncrement()))
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""0000000
-                                                              1000000
-                                                              0000002
-                                                              0000000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0000000
+                                                               1000000
+                                                               0000002
+                                                               0000000""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""0000000
+                                                               1000000
+                                                               0000002
+                                                               0000000""")
         when:
-        board.nextCycle()
+        boardM.nextCycle()
+        boardV.nextCycle()
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""0100000
-                                                              0000020
-                                                              0000000
-                                                              0000000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0100000
+                                                               0000020
+                                                               0000000
+                                                               0000000""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""0000000
+                                                               0000002
+                                                               1000000
+                                                               0000000""")
         when:
-        board.nextCycle()
+        boardM.nextCycle()
+        boardV.nextCycle()
         then:
-        BoardTestDisplay.toString(board, render()) == trim("""0010200
-                                                              0000000
-                                                              0000000
-                                                              0000000""")
+        BoardTestDisplay.toString(boardM, render()) == trim("""0010200
+                                                               0000000
+                                                               0000000
+                                                               0000000""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""0000000
+                                                               1000000
+                                                               0000002
+                                                               0000000""")
+        when:
+        boardM.nextCycle()
+        boardV.nextCycle()
+        then:
+        BoardTestDisplay.toString(boardM, render()) == trim("""0001200
+                                                               0000000
+                                                               0000000
+                                                               0000000""")
+        BoardTestDisplay.toString(boardV, render()) == trim("""0000000
+                                                               0000002
+                                                               1000000
+                                                               0000000""")
     }
 
     Render render() {
